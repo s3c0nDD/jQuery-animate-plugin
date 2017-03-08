dfaa51639dba6bab22039fa843f71ca4dbb629e6
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-08 22:29:19 +0100

    progress git add .git add .!

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,16 +1,38 @@
 /******************** PLUGIN **********************/
 
 (function ( $ ) {
-    
+    /**
+     * function animateMe(animType, animTime, animDelay)
+     * @param animType: string from: ['fadeIn', 'fadeOut']
+     * @param animType: number > 0
+     * @param animDelay: number >= 0
+     * 
+     */
     $.fn.animateMe = function (animType, animTime, animDelay) {
-        console.log('animation start');
-        
-        this.each(function functionName() {
-            // TODO: animate
-        });
+        // Check if animation type is available
+        var availableAnims = [
+            'fadeIn',
+            'fadeOut'
+            // TODO: can add another if wanna to
+        ]
+        if ( $.inArray(animType, availableAnims) === -1 )
+            return $.error('Not available animation type!'); 
 
-        console.log('animation ends');
+        // Check if animation time arguments makes sense
+        if ( typeof (animTime) !== 'number' || typeof (animDelay) !== 'number' ) 
+            return $.error('Animation times types are wrong (required: number)');
         
+        if ( !(animTime > 0) || !(animDelay >= 0) ) 
+            return $.error('Wrong animation times!')
+        
+        // NOW WE CAN (try) REALLY Animate 
+        this.each(function () {
+            var self = this,
+                $children = $(this).children();
+            //TODO: animate here
+        });
+        
+        // chaining stupid!
         return this;
     };
 }( jQuery ));
@@ -21,6 +43,6 @@
 
 $(document).ready(function() {
 
-    $('.list').animateMe();
+    $('.list').animateMe('fadeIn', 500, 200);
     
 });
