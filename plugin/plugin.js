4319e1d07b1a5bc002ba82b6cf49785706fd5951
s3c0nDD <s3c0nDD@users.noreply.github.com>
2017-03-10 22:09:35 +0100

    iteration: plugin fires on childrens (not on parent), plugin nearly complete

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -12,24 +12,17 @@
         var self = this;
 
         if (typeof animName === 'string') {
-
-            self.each(function () {
-                var el = self, 
-                    $children = $(el).children();
+        
+            $(self).children().each(function () {
 
                 window['$'](self)[animName](config);
-                
-                $children.each(function () {
-                    // TODO
-                });
-                
             });
-
+                
         } else {
             console.log("some error");
         }
         
-        return this;
+        return self;
     };
 }( jQuery ));
 
@@ -40,9 +33,9 @@
 $(document).ready(function() {
     
     $('#list1').animateMe('fadeOut',configs[0]).animateMe('fadeIn',configs[0]);
-    $('#list2').animateMe('fadeOut',configs[1]);
+    // $('#list2').animateMe('fadeOut',configs[1]);
     // $('#list3').animateMe('fadeOut',configs[2]);    
-    $('h1').animateMe('css',configs[2])
+    $('.title').animateMe('css',configs[2])
         
 
     // console.log( window['$']('.list')['fadeOut']('slow') )
@@ -50,7 +43,7 @@ $(document).ready(function() {
 });
 
 var configs = [
-    { duration: 3000 },
+    { duration: 1000 },
     { duration: 4000 },
     { color: 'red' }
 ];
